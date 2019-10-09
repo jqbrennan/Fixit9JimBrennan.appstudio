ff5de5f542ccbfcdf691a2e7577eff4547c869c9
Jim Brennan <jimbrennan@creighton.edu>
2019-10-09 15:55:14 -0500

    First commit

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
 /*
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+btnMember.onclick=function(){
+  let memberName = inptFirstName.value
+  let memberCheck = members.includes(memberName)
+  if (memberCheck == true) {
+    lblMember.value = "You are already a member"
+  } else if (memberCheck == false) {
+    lblMember.value = "You have been added as a member"
+    members.unshift(memberName)
+  }
+}
+*/
